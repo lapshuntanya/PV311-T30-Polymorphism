4bb05478ba0ca565a3c5d9b7bab097faf4731d09
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-07 20:11:28 +0300

    Teacher.h

---
+++ PV311-T30-Polymorphism/Teacher.cpp
@@ -0,0 +1,36 @@
+#include "Teacher.h"
+
+Teacher::Teacher()
+{
+}
+
+Teacher::Teacher(string name, float salary, int hours, float price)
+{
+}
+
+void Teacher::setHours(int hours)
+{
+}
+
+void Teacher::setPrice(float price)
+{
+}
+
+int Teacher::getHours() const
+{
+    return 0;
+}
+
+float Teacher::getPrice() const
+{
+    return 0.0f;
+}
+
+void Teacher::showInfo() const
+{
+}
+
+float Teacher::calcSalary() const
+{
+    return 0.0f;
+}
---
+++ PV311-T30-Polymorphism/Teacher.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Employee.h"
+class Teacher :   public Employee
+{
+protected:
+	int hours;
+	float price;
+public:
+	Teacher();
+	Teacher(string name, float salary, int hours, float price);
+
+	void setHours(int hours);
+	void setPrice(float price);
+
+	int getHours()const;
+	float getPrice()const;
+
+	void showInfo()const;
+	float calcSalary()const;
+};
+
