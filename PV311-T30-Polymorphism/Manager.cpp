679076564495ef4439d441785027a635892b3895
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-07 20:05:55 +0300

    Manager.cpp

--- PV311-T30-Polymorphism/Manager.cpp
+++ PV311-T30-Polymorphism/Manager.cpp
@@ -2,35 +2,29 @@
 
 Manager::Manager()
 {
+    percent = 3;
+    sales = 0;
 }
 
 Manager::Manager(string name, float salary, int percent, float sales)
+    :Employee(name, salary)
 {
+    this->percent = percent;
+    this->sales = sales;
 }
 
-void Manager::setPercent(int percent)
-{
+void Manager::setPercent(int percent){ this->percent = percent; }
+void Manager::setSale(float sales) { this->sales = sales; }
+
+int Manager::getPercent() const{    return percent;}
+float Manager::getSales() const{    return sales;}
+
+void Manager::showInfo() const{
+    Employee::showInfo();
+    cout << "Percent: " << percent << "%\n";
+    cout << "Sales: " << sales << "UAH\n";
 }
 
-void Manager::setSale(float sales)
-{
-}
-
-int Manager::getPercent() const
-{
-    return 0;
-}
-
-float Manager::getSales() const
-{
-    return 0.0f;
-}
-
-void Manager::showInfo() const
-{
-}
-
-float Manager::calcSalary() const
-{
-    return 0.0f;
+float Manager::calcSalary() const{
+    return (salary + sales* percent/100) * 0.85;
 }
