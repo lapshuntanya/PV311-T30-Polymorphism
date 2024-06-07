34b2ba2167db269cf164f9b72e0ca0297f3bea94
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-07 20:51:35 +0300

    main

--- PV311-T30-Polymorphism/PV311-T30-Polymorphism.cpp
+++ PV311-T30-Polymorphism/PV311-T30-Polymorphism.cpp
@@ -1,8 +1,23 @@
-
-#include <iostream>
+#include "Employee.h"
+#include "Manager.h"
+#include "Teacher.h"
+#include <vector>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Employee obj1("Бердник Даніїл", 9'000);
+    Manager obj2("Білецький Віталій", 10'000, 10, 200'000);
+    Teacher obj3("Будянський Данило", 15'000, 10, 500);
+
+    vector<Employee*> company{
+        &obj1, &obj2, &obj3,
+        new Manager("Дехтяренко Олег", 10'000, 15, 150'000),
+        new Teacher("Левченко Ярослав", 20'000, 20, 550),
+        new Manager("Місенко Артур", 15'000, 25, 100'000)
+    };
+
+
+
+    return 0;
 }
 
